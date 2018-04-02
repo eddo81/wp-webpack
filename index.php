e676786e1365a489d9790559fa273dbfff9b1529
eddo81 <eduardo_jonnerstig@live.com>
2018-04-02 23:17:54 +0200

    removed assets.php

--- index.php
+++ index.php
@@ -1,6 +1,2 @@
-<?php /*include_once('views/pages/test.php');*/ ?>
-<?php //include_once('app/views/layouts/master.php'); ?>
-
 <?php include_once('app/views/layouts/head.php'); ?>
 <?php include_once('app/views/layouts/footer.php'); ?>
-

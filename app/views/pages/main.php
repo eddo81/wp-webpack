71634034607ab82ef225745739df62b5f8b0e976
Eduardo Jönnerstig <eduardo@iMacHome.local>
2018-06-01 11:33:09 +0200

    cleaned branch

--- app/views/pages/main.php
+++
@@ -1,2 +0,0 @@
-<?php  ?>
-<main id="app"></main>
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php include_once('app/views/layouts/head.php'); ?>
-<?php include_once('app/views/pages/main.php'); ?>
-<?php include_once('app/views/layouts/footer.php'); ?>
